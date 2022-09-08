e228deaaf1bd074dc6d642782fb56f3ba40829e2
theGeekkit <sonofgondolin16@gmail.com>
2022-09-08 09:43:53 -0500

    9-7-22

--- built-in-methods.js
+++ built-in-methods.js
@@ -1,2 +1,23 @@
-let people = ["John", "James", "Jimmy"];
-console.log("There are three peope.")
+// let people = ["John", "James", "Jimmy", "Jane"];
+// console.log(`There are ${people.length} peope.`)
+
+let people = {
+    0: 'John',
+    1: "James",
+    2: "Jimmy",
+    3: "Jane",
+    length: 4,
+    map: function(callback){
+        let newArr = [];
+        for(let i = 0; i < this.length; i++){
+            newArr.push(callback(this[i]))
+        }
+        return newArr;
+    }
+}
+
+// console.log(`There are ${people.length} people.`);
+
+let introductions = people.map((person)=>{
+    return 'My name is ${person}.'
+})
