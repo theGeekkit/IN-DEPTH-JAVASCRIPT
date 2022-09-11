591e64596612c0fbcc8506f9901ff5a4f27f7589
theGeekkit <sonofgondolin16@gmail.com>
2022-09-11 18:45:35 -0500

    module type 9-11-22

---
+++ pages/settings.js
@@ -0,0 +1,13 @@
+class Settings{
+    constructor(){
+        this.render();
+
+    }
+
+    render(){
+        let appElem = document.getElementById("app");
+        appElem.innerHTML += `
+            <h1 class='text-center'>Settings</h1>
+        `
+    }
+}
